70f15e57ef2a7d4a0b84eaba9ad5c7fd353d43a6
= <=>
2015-10-01 20:50:08 -0500

    Adds title to the site.

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 	<link href='http://fonts.googleapis.com/css?family=PT+Serif:400,700,700italic,400italic' rel='stylesheet' type='text/css'>
